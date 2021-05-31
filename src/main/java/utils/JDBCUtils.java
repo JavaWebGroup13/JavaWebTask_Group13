d8310b2d730d2df0f8b912e89440b1955f0d7fac
renhanlu <951768725@qq.com>
2021-05-31 22:24:48 +0800

    JDBCUtils.java

--- src/main/java/utils/JDBCUtils.java
+++ src/main/java/utils/JDBCUtils.java
@@ -1,5 +1,47 @@
 package utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCUtils {
-
+	public static Connection getConnerction() throws SQLException,
+	ClassNotFoundException{
+		Class.forName("com.mysql.jdbc.Driver");
+		String url="jdbc:mysql://localhost:3306/jdbc";
+		String username="root";
+		String password="itcast";
+		Connection conn=DriverManager.getConnection(url,username,password);
+		return conn;	
+	}
+   public static void relesae(Statement stmt,Connection conn) {
+	   if(stmt!=null) {
+		   try {
+			   stmt.close();
+		   }catch(SQLException e) {
+			   e.printStackTrace();
+		   }
+		  stmt=null;
+	   }
+	   if(conn!=null) {
+		   try {
+			   conn.close();
+		   }catch(SQLException e) {
+			   e.printStackTrace();
+		   }
+		   conn=null;
+	   }
+   }
+   public static void relesae(ResultSet rs,Statement stmt,Connection conn) {
+	   if(rs!=null) {
+		   try {
+			   rs.close();
+		   }catch(SQLException e) {
+			   e.printStackTrace();
+		   }
+		   rs=null;
+	   }
+	   relesae(stmt,conn);
+   }
 }
